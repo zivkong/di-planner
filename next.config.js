0b479118918484f917c39a47cbc5fc1abec16f5d
Ziv Kong <zivkong@gmail.com>
2022-09-06 11:09:20 +0800

    Nest Config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,12 @@ const isProduction = !!(process.env.NODE_ENV === 'production')
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  assetPrefix: isProduction ? "/di-planner" : ""
+  swcMinify: true
+}
+
+if (isProduction) {
+  nextConfig["basePath"] = "/di-planner"
+  nextConfig["assetPrefix"] = "/di-planner/"
 }
 
 module.exports = nextConfig
