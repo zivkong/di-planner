55ae25c8ff5a67679964a80469f7cbec4e8009b4
Ziv Kong <zivkong@gmail.com>
2022-09-06 10:49:51 +0800

    Resolve base path

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  basePath: '/di-planner'
+  swcMinify: true
 }
+
+if (process.env.NODE_ENV === 'production') nextConfig["basePath"] = "/di-planner"
 
 module.exports = nextConfig
