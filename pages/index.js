307abde125152ee99f1dff72e73c662339154b2e
Ziv Kong <zivkong@gmail.com>
2022-09-06 11:21:18 +0800

    Test Base Path

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to Diablo Immortal Clan Planner
         </h1>
+        <Link href="/login">Login</Link>
       </main>
 
       <footer className={styles.footer}>
---
+++ pages/login/index.js
@@ -0,0 +1,7 @@
+const Login = () => {
+  return (
+    <div>This is login</div>
+  )
+}
+
+export default Login
